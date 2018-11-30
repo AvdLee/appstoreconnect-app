8d55b319e47f08a31c17593117532428b83aee6b
Antoine van der Lee <antoine@wetransfer.com>
2018-11-30 23:33:11 +0100

    Synced width for menus

--- AppStoreConnect/Menu/MenuViewController.swift
+++ AppStoreConnect/Menu/MenuViewController.swift
@@ -24,7 +24,7 @@ final class MenuViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        view.widthAnchor.constraint(equalToConstant: 200).isActive = true
 
         provider.request(.apps()) { [weak self] (result) in
             DispatchQueue.main.async {
